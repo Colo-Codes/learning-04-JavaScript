dcab01161dff5a710ad495792b863a3705c1a9a6
Darthcolo <damian.demasi.1@gmail.com>
2020-03-12 15:15:25 +1030

    DOM Events - ul

--- 53-JavaScript-DOM-Events.js
+++ 53-JavaScript-DOM-Events.js
@@ -2,8 +2,15 @@
 
 var button = document.querySelector('button');
 var paragraph = document.querySelector('p');
+var ul = document.querySelector(ul);
 
-// Click listener
+// Click listener on button:
 button.addEventListener('click', function(){
     paragraph.textContent = 'Someone has clicked the button!';
+});
+// If I create another listener targetting the same element, it will also apply.
+
+// Click listener on ul:
+ul.addEventListener('click',function(){
+    ul.style.color = 'red';
 });
