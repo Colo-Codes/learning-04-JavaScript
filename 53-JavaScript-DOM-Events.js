778b185ee9291d3804548f7ded218988cf55ad04
Darthcolo <damian.demasi.1@gmail.com>
2020-03-12 15:06:40 +1030

    DOM Events - Button

--- 53-JavaScript-DOM-Events.js
+++ 53-JavaScript-DOM-Events.js
@@ -1,1 +1,9 @@
-console.log('Connected...');
+// console.log('Connected...');
+
+var button = document.querySelector('button');
+var paragraph = document.querySelector('p');
+
+// Click listener
+button.addEventListener('click', function(){
+    paragraph.textContent = 'Someone has clicked the button!';
+});
