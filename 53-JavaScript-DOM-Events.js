d22e971992e093e5ee9ed7ddbadbe628f90036fb
Darthcolo <damian.demasi.1@gmail.com>
2020-03-12 15:45:39 +1030

    DOM Events - ol li and this keyword

--- 53-JavaScript-DOM-Events.js
+++ 53-JavaScript-DOM-Events.js
@@ -3,15 +3,16 @@
 var button = document.querySelector('button');
 var paragraph = document.querySelector('p');
 var ul = document.querySelector('ul');
+var olLi = document.querySelectorAll('ol li');
 
 // Click listener on button:
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     paragraph.textContent = 'Someone has clicked the button!';
 });
 // If I create another listener targetting the same element, it will also apply.
 
 // Click listener on ul:
-ul.addEventListener('click', function(){
+ul.addEventListener('click', function () {
     // Toggle logic:
     if (ul.style.color == 'red') {
         ul.style.color = 'blue';
@@ -19,3 +20,17 @@ ul.addEventListener('click', function(){
         ul.style.color = 'red';
     }
 });
+
+// Click listener on ol li (not using an anonymous function):
+function changetext() {
+    // Toggle logic:
+    if (this.style.color == 'green') {
+        this.style.color = 'cyan';
+    } else {
+        this.style.color = 'green';
+    }
+}
+
+for (i = 0; i < olLi.length; i++) {
+    olLi[i].addEventListener('click', changetext);
+}
