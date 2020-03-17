90354e76e8518140e42eff0d660c5a8d01fab4f3
Darthcolo <damian.demasi.1@gmail.com>
2020-03-17 17:19:48 +1030

    jQuery: Events

---
+++ 59-JavaScript-jQuery-Events.js
@@ -0,0 +1,11 @@
+// console.log('Connected...');
+// console.log(jQuery);
+
+// Event: click()
+var click1 = $('button').click(function() {
+    $(this).css('background', 'pink');
+    console.log('A button was clicked: ' + $(this).text());
+    
+});
+
+
