fbfa00e304d9fb81695eff15612584cfe158b30a
Darthcolo <damian.demasi.1@gmail.com>
2020-03-19 07:56:25 +1030

    v0.1

--- 61-jQuery-ToDo-project/assets/js/todos.js
+++ 61-jQuery-ToDo-project/assets/js/todos.js
@@ -1,2 +1,10 @@
 // Testing if the file is correctly linked
-console.log('Connected...');
+// console.log('Connected...');
+
+// Check off specific to-dos by clicking
+$('li').click(function(){
+    $(this).css({
+        // grey
+        // linethrough
+    });
+});
