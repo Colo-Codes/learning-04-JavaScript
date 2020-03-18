992a5dfb843fdabcfc54ab82fef8203d28c687a5
Darthcolo <damian.demasi.1@gmail.com>
2020-03-19 04:29:30 +1030

    jQuery Effects x 3

--- 60-JavaScript-jQuery-Effects.js
+++ 60-JavaScript-jQuery-Effects.js
@@ -1,11 +1,28 @@
 // console.log('Connected...');
 // console.log(jQuery);
 
-$('button').on('click', function() {
-    $('div').fadeOut(1000, function(){
+// fadeOut()
+$('.buttonOut').on('click', function() {
+    $('.divOut').fadeOut(2000, function(){
         console.log('This text will show after the fadeOut is completed, because is inside a callback function inside the fadeOut method.');
         // To remove a faded out element:
         $(this).remove();
+    });
+    console.log('This line will show before the fadeOut is completed, because JS doesn\'t wait for the previous line of code to be completed.');
+});
+
+// fadeIn()
+$('.buttonIn').on('click', function() {
+    $('.divIn').fadeIn(2000, function(){
+        console.log('This text will show after the fadeOut is completed, because is inside a callback function inside the fadeOut method.');
+    });
+    console.log('This line will show before the fadeOut is completed, because JS doesn\'t wait for the previous line of code to be completed.');
+});
+
+// fadeToggle()
+$('.buttonToggle').on('click', function() {
+    $('.divToggle').fadeToggle(2000, function(){
+        console.log('This text will show after the fadeOut is completed, because is inside a callback function inside the fadeOut method.');
     });
     console.log('This line will show before the fadeOut is completed, because JS doesn\'t wait for the previous line of code to be completed.');
 });
