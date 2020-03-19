ffb7471ad2182218ae1eb403383f39adaf688dd8
Darthcolo <damian.demasi.1@gmail.com>
2020-03-19 16:18:45 +1030

    v0.4

--- 61-jQuery-ToDo-project/assets/js/todos.js
+++ 61-jQuery-ToDo-project/assets/js/todos.js
@@ -2,7 +2,7 @@
 // console.log('Connected...');
 
 // Check off specific to-dos by clicking
-$('li').click(function(){
+$('ul').on('click', 'li', function(){ // Using click() will only work for old elements, not for new ones. That's why we use on(), on the parent element who was on the page when it was loaded
     // // console.log($(this).css("color"));
     // if ($(this).css('color') === 'rgb(128, 128, 128)') { // rgb(128, 128, 128) == grey
     //     $(this).css({
@@ -20,9 +20,22 @@ $('li').click(function(){
 });
 
 // Click on icon to delete to-to
-$('span').click(function(event){
+$('ul').on('click', 'span', function(event){ // Using click() will only work for old elements, not for new ones. That's why we use on(), on the parent element who was on the page when it was loaded
     $(this).parent().fadeOut(500, function() {
         $(this).remove(); // The $(this) here is referencing to the $(this).parent() from before (the element on which the fadeOut() is being applied)
     });
     event.stopPropagation(); // This is to prevent the Event Bubbling
+});
+
+// Enter a new to-do in the text-box
+$('input[type="text"]').keypress(function(event) {
+    // console.log(event);
+    if (event.which === 13) {
+        // console.log($(this).val());
+        var toDoElement = $(this).val();
+        $(this).val('');
+        $('ul').append('<li><span>X</span>' + toDoElement + '</li>');
+    } else {
+        console.log('You pressed something different to ENTER...');
+    }
 });
