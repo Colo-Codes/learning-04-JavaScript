f1bd6eb1cfef57c6c7a62e4ee6f1bf75d23b34ac
Darthcolo <damian.demasi.1@gmail.com>
2020-03-19 15:10:50 +1030

    v0.2

--- 61-jQuery-ToDo-project/assets/js/todos.js
+++ 61-jQuery-ToDo-project/assets/js/todos.js
@@ -3,8 +3,18 @@
 
 // Check off specific to-dos by clicking
 $('li').click(function(){
-    $(this).css({
-        // grey
-        // linethrough
-    });
+    // // console.log($(this).css("color"));
+    // if ($(this).css('color') === 'rgb(128, 128, 128)') { // rgb(128, 128, 128) == grey
+    //     $(this).css({
+    //         'color': 'black',
+    //         'text-decoration': 'none',
+    //     });
+    // } else {
+    //     $(this).css({
+    //         'color': 'grey',
+    //         'text-decoration': 'line-through',
+    //     });
+    // }
+    // Instead of using the if statement and logic, we can toggle a class:
+    $(this).toggleClass('completed');
 });
